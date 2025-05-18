a70a41ea470e880f04c9ea7444be5ddea28c4623
Joe <mlgxjofish@gmail.com>
2025-05-18 23:20:38 +0100

    bookbot V1.2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+['main.py', 'books/frankenstein.txt']
 def get_book_text(filepath):
 
     with open(filepath) as f:
@@ -11,7 +12,7 @@ from stats import char_count
 from stats import dict_sorter
 
 def main():
-    path ="books/frankenstein.txt"
+    path =""
     book_text = get_book_text(path)
     num_words = word_count(book_text)
     character_counts = char_count(book_text)
