affebfc1e100fbd5dd3f81d0248e623a6a3823f4
Joe <mlgxjofish@gmail.com>
2025-05-18 21:23:26 +0100

    bookbot V1.1

--- main.py
+++ main.py
@@ -8,16 +8,29 @@ def get_book_text(filepath):
 
 from stats import word_count
 from stats import char_count
-
+from stats import dict_sorter
 
 def main():
-    
-    book_text = get_book_text("books/frankenstein.txt")
+    path ="books/frankenstein.txt"
+    book_text = get_book_text(path)
     num_words = word_count(book_text)
     character_counts = char_count(book_text)
-    print(f"{num_words} words found in the document")
-    print (character_counts)
+    sorted_list = dict_sorter(character_counts)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
 
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+
+    for item in sorted_list:
+        char = item["char"]
+        count = item["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -18,3 +18,17 @@ def char_count(book_text):
             character_counts[c] +=1
 
     return character_counts
+
+def sort_on(dict):
+    return dict["num"]
+
+def dict_sorter(character_counts):
+    results = []
+
+    for char, count in character_counts.items():
+        results.append({"char": char, "num": count})
+
+    results.sort(reverse=True, key=sort_on)
+    
+
+    return results
